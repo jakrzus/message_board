2ca49035bd8449b692a1f23a5e972cf2984dc15f
JKrzus <jakub.krzus@gmail.com>
2018-08-14 08:47:16 +0100

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+require 'database_cleaner'
+
+DatabaseCleaner.clean_with(:truncation)
+
+10.times do
+  email = Faker::Internet.email
+  password = 'qwertyqwerty'
+
+  user = User.new
+  user.email = email
+  user.password = password
+  user.password_confirmation = password
+  user.save!
+  10.times do
+    title = Faker::Lorem.sentence
+    tekst = Faker::Lorem.paragraphs
+
+    post = user.posts.new
+    post.title = title
+    post.tekst = tekst
+    post.save!
+    10.times do
+      comment = Faker::Lorem.paragraphs
+      reply = post.replies.new
+      reply.comment = comment
+      reply.user = user
+      reply.save!
+    end
+  end
+end
